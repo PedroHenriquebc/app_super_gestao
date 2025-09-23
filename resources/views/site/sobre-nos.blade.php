8e6e883961072152efa39feb1d183b8c7a6597ee
Your Name <you@example.com>
2025-09-22 21:56:37 -0300

    feat: modifica rotas

---
+++ resources/views/site/sobre-nos.blade.php
@@ -0,0 +1,7 @@
+<h3>Sobre nós (view)</h3>
+
+<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="contato">Contato</a></li>
+    <li><a href="sobre-nos">Sobre Nós</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\SobreNosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('contato', [ContatoController::class, 'contato'])->name('site.conato');
+Route::get('sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('login', function(){return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('produtos', function(){return 'Produtos';})->name('app.produtos');
 });
+
+
+// Route::get('sobre-nos/{nome}/{categoria?}', function(string $nome, int $categoria = 0){
+//     return "Olá $nome - Categoria: $categoria";
+// })->where('categoria','[0-9]+')->where('nome', '[a-zA-Z]+');
