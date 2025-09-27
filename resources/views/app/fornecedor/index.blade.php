f89cd4ac97c7ba7d952bf4b5ba5663fe93616cb0
Your Name <you@example.com>
2025-09-27 19:55:49 -0300

    feat: inclui comentários e tags php puro no blade

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,9 @@
+<h3>View de Fornecedor</h3>
+
+{{-- impressão de algo no blade --}}
+{{ 'Impressão com chaves duplas no blade' }}
+
+@php
+    echo '<br>';
+    echo 'Impressão com echo dentro de tags php no blade';
+@endphp
