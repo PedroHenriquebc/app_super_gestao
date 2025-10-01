6b4a339e62c64272e4b004b4f41d9dba72f59480
Pedro Henrique <pedrohbc96@gmail.com>
2025-09-30 21:52:01 -0300

    feat: @isset blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -8,15 +8,27 @@
     // echo 'Impressão com echo dentro de tags php no blade';
 @endphp
 
-@if (count($fornecedores) > 0 and count($fornecedores) < 10)
-    <h3>Existem alguns Fornecedores</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem muitos Fornecedores</h3>
-@else
-    <h3>Ainda não existem Fornecedores</h3>
-@endif
+@isset($fornecedores)
+    @if (count($fornecedores) > 0 and count($fornecedores) < 10)
+        <h3>Existem alguns Fornecedores</h3>
+    @elseif(count($fornecedores) > 10)
+        <h3>Existem muitos Fornecedores</h3>
+    @else
+        <h3>Ainda não existem Fornecedores</h3>
+    @endif
 
-{{-- Entra na condicional se for falso(contrário do if) --}}
-@unless ($fornecedores[0]['status'] == 'S')
-    <h3>Fornecedor Inativo</h3>
-@endunless
+    {{-- Entra na condicional se for falso(contrário do if) --}}
+    @unless ($fornecedores[0]['status'] == 'S')
+        <h3>Fornecedor Inativo</h3>
+    @endunless
+
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}    
+    @endisset
+    
+
+@endisset
